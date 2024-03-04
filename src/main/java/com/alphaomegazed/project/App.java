d36963ab4e97a7ca8ea0cc48837ed9e3249bed37
Brainmated <alsynetos@hotmail.com>
2024-03-04 20:09:21 +0200

    Initi commit

---
+++ src/main/java/com/alphaomegazed/project/App.java
@@ -0,0 +1,13 @@
+package com.alphaomegazed.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
