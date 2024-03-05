fff279bd4f3e992b16be99207e2050a95b51f16c
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2024-03-05 11:18:49 +0200

    Remade alphaomegazed-apartments
    Added new Maven dependencies, set connection with SQL server, performed build tests.

---
+++ aoz_apartments/src/main/java/com/alphaomegazed/aoz_apartments/AozApartmentsApplication.java
@@ -0,0 +1,13 @@
+package com.alphaomegazed.aoz_apartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AozApartmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AozApartmentsApplication.class, args);
+	}
+
+}
---
+++ aoz_apartments/src/test/java/com/alphaomegazed/aoz_apartments/AozApartmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphaomegazed.aoz_apartments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AozApartmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
