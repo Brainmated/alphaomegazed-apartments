d247847d0957a7995c40f846f284185f2ee3af0f
Alex Synetos Konstantinidis <93430275+Brainmated@users.noreply.github.com>
2024-03-10 12:53:42 +0200

    Documented AuthenticationService.java

--- aoz_apartments/src/main/java/com/alphaomegazed/aoz_apartments/service/AuthenticationService.java
+++ aoz_apartments/src/main/java/com/alphaomegazed/aoz_apartments/service/AuthenticationService.java
@@ -11,6 +11,16 @@ import com.alphaomegazed.aoz_apartments.model.AuthenticationResponse;
 import com.alphaomegazed.aoz_apartments.model.UserModel;
 import com.alphaomegazed.aoz_apartments.repository_interfaces.UserRepository;
 
+/*
+#Overview
+This service class provided methods for registering new users and authenticating existing ones.
+It users UserRepository for persistence and AuthenticationManager for user Authentication.
+
+#Standout variables
+'userRepository' is the repository interface to handle CRUD operations for the 'UserModel' entity.
+'passwordEncoder' is the component that encodes plain text passwords.
+'authenticationManager' is the Spring Security interface for authenticating 'Authentication' objects.
+*/
 @Service
 public class AuthenticationService {
 
@@ -27,6 +37,13 @@ public class AuthenticationService {
         this.authenticationManager = authenticationManager;
     }
 
+    /*
+    #Registers a new user with the provided user details.
+    #Encodes the password using PasswordEncoder.
+    #Persists the new user object using the UserRepository interface.
+    #Generates a JWT token for the newly registered user using 'JwtService'.
+    #Return the JWT token through the AuthenticationResponse.
+    */
     public AuthenticationResponse register(UserModel request) {
         UserModel user = new UserModel();
         user.setUsername(request.getUsername());
@@ -40,6 +57,12 @@ public class AuthenticationService {
         return new AuthenticationResponse(token);
     }
 
+    /*
+    #Authenticates the user using AuthenticationManager with the provided username and password.
+    #Fetches the usernames from the UserRepository.
+    #Throws exception if the user cant be found.
+    #Returns the JWT token.
+    */
     public AuthenticationResponse authenticate(UserModel request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
